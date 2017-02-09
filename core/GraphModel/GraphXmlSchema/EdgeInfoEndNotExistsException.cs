c91882168c2cc8ec3fc37d7689348b9f2e4726f8
Yair Shohat <yairsno1@gmail.com>
2017-02-09 11:32:44 +0200

    Add files via upload

--- core/GraphModel/GraphXmlSchema/EdgeInfoEndNotExistsException.cs
+++ core/GraphModel/GraphXmlSchema/EdgeInfoEndNotExistsException.cs
@@ -8,23 +8,22 @@ using System.Threading.Tasks;
 namespace YS.Training.Core.GraphModel.GraphXmlSchema
 {
   [Serializable]
-  public class VertexInfoEmptyNameException : Exception
+  public class EdgeInfoEndNotExistsException : Exception
   {
-
-    public VertexInfoEmptyNameException() : base()
+    public EdgeInfoEndNotExistsException() : base()
     {
     }
 
-    public VertexInfoEmptyNameException(string p_message) : base(p_message)
+    public EdgeInfoEndNotExistsException(string p_message) : base(p_message)
     {
     }
 
-    public VertexInfoEmptyNameException(string p_message, Exception p_innerException) : 
+    public EdgeInfoEndNotExistsException(string p_message, Exception p_innerException) : 
          base (p_message, p_innerException)
     {
     }
 
-    protected VertexInfoEmptyNameException(SerializationInfo p_info, StreamingContext p_context) :
+    protected EdgeInfoEndNotExistsException(SerializationInfo p_info, StreamingContext p_context) :
       base(p_info, p_context)
     {
     }
