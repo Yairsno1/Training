a7e5d2a566185b65c8a943ad8d8b7f812dcb7d42
Yair Shohat <yairsno1@gmail.com>
2017-02-09 17:47:05 +0200

    Add files via upload

---
+++ core/GraphModel/GraphXmlSchema/EdgeInfoEndDuplicationException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YS.Training.Core.GraphModel.GraphXmlSchema
+{
+  [Serializable]
+  public class EdgeInfoEndDuplicationException : Exception
+  {
+    public EdgeInfoEndDuplicationException() : base()
+    {
+    }
+
+    public EdgeInfoEndDuplicationException(string p_message) : base(p_message)
+    {
+    }
+
+    public EdgeInfoEndDuplicationException(string p_message, Exception p_innerException) : 
+         base (p_message, p_innerException)
+    {
+    }
+
+    protected EdgeInfoEndDuplicationException(SerializationInfo p_info, StreamingContext p_context) :
+      base(p_info, p_context)
+    {
+    }
+  }
+}
