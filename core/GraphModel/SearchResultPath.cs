cedebf99ac9841725d0bc286affa26a645fc3c37
Yair Shohat <yairsno1@gmail.com>
2017-02-12 17:40:45 +0200

    Add files via upload

---
+++ core/GraphModel/SearchResultPath.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YS.Training.Core.Interfaces.GraphModelDef;
+
+namespace YS.Training.Core.GraphModel
+{
+  internal class SearchResultPath : IPath
+  {
+    private LinkedList<int> xx;
+
+    public SearchResultPath()
+    {
+
+    }
+
+
+       #region IPath
+    public IVertex EndVertex
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public bool IsEmpty
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public IVertex NextVertex(IVertex sourceVertex)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IVertex StartVertex
+    {
+      get { throw new NotImplementedException(); }
+    }
+
+    public double Weight
+    {
+      get { throw new NotImplementedException(); }
+    }
+      #endregion
+
+  }
+}
---
+++ core/GraphModel/SearchVertex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YS.Training.Core.Interfaces.GraphModelDef;
+
+namespace YS.Training.Core.GraphModel
+{
+  internal class SearchVertex : IComparable<SearchVertex>
+  {
+    private IVertex m_subject;
+    private SearchVertex m_prev;
+    private double m_delta;
+    private double m_approximation;
+
+    public SearchVertex(IVertex p_subject,
+                          SearchVertex p_expandedFrom,
+                          double p_expansionDelta,
+                          double p_approximation)
+    {
+      m_subject = p_subject;
+      m_prev = p_expandedFrom;
+      m_delta = (null == m_prev) ? p_expansionDelta : m_prev.m_delta + p_expansionDelta;
+      m_approximation = p_approximation;
+    }
+
+    public SearchVertex Prev
+    {
+      get
+      {
+        return m_prev;
+      }
+    }
+
+    public int CompareTo(SearchVertex p_other)
+    {
+      return (m_delta + m_approximation).CompareTo(p_other.m_delta + p_other.m_approximation);
+    }
+  }
+}
